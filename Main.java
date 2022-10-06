94e583c75a99d53a6493d6a45846d1e79b51c02f
Metehan Saçakçı <metehan.sacakci@ug.bilkent.edu.tr>
2022-10-06 19:36:43 +0300

    Test code of decorator pattern is added to main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        ConsoleLogger logger = new ConsoleLogger();
-        logger.log("Hello world"); // Prints 'Hello world'
+        String message = "Hello world";
+
+        Logger rawLogger = new ConsoleLogger();
+        rawLogger.log(message); // Hello world
+
+        Logger decoratedLogger_1 = new TimestampLogger(rawLogger);
+        decoratedLogger_1.log(message); // Thu Oct 06 18:12:24 TRT 2022 - Hello world
+
+        Logger decoratedLogger_2 = new EncryptionLogger(rawLogger);
+        decoratedLogger_2.log(message); // Jgnnq yqtnf
+
+        Logger decoratedLogger_3 = new TimestampLogger(new EncryptionLogger(rawLogger));
+        decoratedLogger_3.log(message); // Vjw Qev LR MTVMNVNP VTV NLNN I Jgnnq yqtnf
+
+        Logger decoratedLogger_4 = new EncryptionLogger(new TimestampLogger(rawLogger));
+        decoratedLogger_4.log(message); // Thu Oct 06 18:12:24 TRT 2022 - Jgnnq yqtnf
     }
 }
