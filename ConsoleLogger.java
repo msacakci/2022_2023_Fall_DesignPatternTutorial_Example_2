b3b0deaeea7ee42b9f34f59a53030c6f44bbe2d0
Metehan Saçakçı <metehan.sacakci@ug.bilkent.edu.tr>
2022-10-06 19:28:38 +0300

    Main and ConsoleLogger classes are added

---
+++ ConsoleLogger.java
@@ -0,0 +1,5 @@
+public class ConsoleLogger {
+    public void log(String message) {
+        System.out.println(message);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        ConsoleLogger logger = new ConsoleLogger();
+        logger.log("Hello world"); // Prints 'Hello world'
+    }
+}
